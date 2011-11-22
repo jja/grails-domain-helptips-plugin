115d401fd7c71eaa76d4fe2daecb7cc4a0ad465f
John J. Allison <jja@sinequanon.net>
2011-11-22 15:07:35 -0700

    added index.gsp for testing; syntax/typo fixes

--- DomainHelptipsGrailsPlugin.groovy
+++ DomainHelptipsGrailsPlugin.groovy
@@ -3,8 +3,12 @@ class DomainHelptipsGrailsPlugin {
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
+
     // the other plugins this plugin depends on
-    def dependsOn = ['jquery-ui':'1.8.15 > *']
+    def dependsOn = [:]
+    // for now, we're just including the needed files inline
+    //def dependsOn = ['jquery-ui':'1.8.15 > *']
+
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
--- grails-app/taglib/helptips/HelptipTagLib.groovy
+++ grails-app/taglib/helptips/HelptipTagLib.groovy
@@ -11,25 +11,25 @@ class HelptipTagLib {
         if ( pluginManager.getGrailsPlugin('resources') ) {
             out << "<r:require module=\"jquery-ui\"/>\n"
         } else if (attrs.includeJQ==null || attrs.includeJQ==true) {
-            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-1.7.custom.min.js" ) + "\"></script>\n"
-            out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'jquery-ui-1.8.15.custom.css" ) + "\"/>\n"
-            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-ui-1.8.15.custom.min.js" ) + "\"></script>\n"
+            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-1.7.custom.min.js' ) + "\"></script>\n"
+            out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'jquery-ui-1.8.15.custom.css' ) + "\"/>\n"
+            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-ui-1.8.15.custom.min.js' ) + "\"></script>\n"
           }
         out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery.bt.custom.js' ) + "\"></script>\n"
-        out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'helptips.css" ) + "\"/>\n"
-        out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'helptips.js" ) + "\"></script>\n"
+        out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'helptips.css' ) + "\"/>\n"
+        out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'helptips.js' ) + "\"></script>\n"
         out << "<script type=\"text/javascript\">\n"
 
         out << """
 window.helptips = window.helptips || {};
 var helptips = window.helptips;
 helptips.updateDomainProperty = function(domainPropertyId,domainNameId) {
-  $(function() {
-    $('#'+domainPropertyId).html('');
-    $.ajax({
-        url: '${createLink(controller:"helptips", action:"properties")}' + '/' + $('#'+domainNameId).val(),
+  \$(function() {
+    \$('#'+domainPropertyId).html('');
+    \$.ajax({
+        url: '${createLink(controller:"helptips", action:"properties")}' + '/' + \$('#'+domainNameId).val(),
         success: function(data) {
-            $('#'+domainPropertyId).html(data);
+            \$('#'+domainPropertyId).html(data);
             }
         });
   });
