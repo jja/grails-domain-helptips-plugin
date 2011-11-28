7924cd7b57f59071b7b96b26a54f9ee5c3b2a891
John J. Allison <jja@sinequanon.net>
2011-11-28 11:12:15 -0700

    flush save in bootstrap

--- grails-app/conf/HelptipsBootStrap.groovy
+++ grails-app/conf/HelptipsBootStrap.groovy
@@ -9,19 +9,19 @@ class HelptipsBootStrap {
 
             new Helptip(domainName:'helptip', domainProperty:"domainName", published:true, title:"Domain",
                 description: "<p>Select a domain from the drop down. Note that some of the domains may be ones you don't usually have access to, or are renamed in the application.</p>"
-            ).save()
+            ).save(flush:true)
 
             new Helptip(domainName:'helptip', domainProperty:"domainProperty", published:true, title:"Property",
                 description: '<p>Select a domain property from the drop down. This corresponds to a column in the database table.</p>'
-            ).save()
+            ).save(flush:true)
 
             new Helptip(domainName:'helptip', domainProperty:"title", published:true, title:"Tip Title ",
                 description: "<p>This will be shown as the tip title, in bold or different color.</p>"
-            ).save()
+            ).save(flush:true)
 
             new Helptip(domainName:'helptip', domainProperty:"description", published:true, title:"Tip Description ",
                 description: "<p>The main tip text -- a longer description/explanation than the title.</p>"
-            ).save()
+            ).save(flush:true)
 
             }
 
