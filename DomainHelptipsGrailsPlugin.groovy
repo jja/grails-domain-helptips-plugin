99d923e16871f4143674569d6b99ab673084a73e
John J. Allison <jja@sinequanon.net>
2014-08-07 11:34:42 -0600

    attempt to use resources

--- DomainHelptipsGrailsPlugin.groovy
+++ DomainHelptipsGrailsPlugin.groovy
@@ -1,13 +1,6 @@
 class DomainHelptipsGrailsPlugin {
-    // the plugin version
     def version = "2.0"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.7 > *"
-
-    // the other plugins this plugin depends on
-    //def dependsOn = ['jquery-ui':'1.10.3 > *']
-    // for now, we're just including the needed files inline
-    def dependsOn = [:]
 
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
@@ -23,19 +16,7 @@ Tips are user-editable.
 Inspired by/based on Grails plugin dynamic-help
 '''
 
-    // URL to the plugin's documentation
     //def documentation = "http://grails.org/plugin/domain-helptips"
     def documentation = "https://github.com/jja/domain-helptips/blob/master/README"
 
-    def doWithWebDescriptor = { xml -> }
-
-    def doWithSpring = { }
-
-    def doWithDynamicMethods = { ctx -> }
-
-    def doWithApplicationContext = { applicationContext -> }
-
-    def onChange = { event -> }
-
-    def onConfigChange = { event -> }
 }
--- grails-app/taglib/helptips/HelptipTagLib.groovy
+++ grails-app/taglib/helptips/HelptipTagLib.groovy
@@ -8,16 +8,25 @@ class HelptipTagLib {
     if (attrs.domain) {
       def helptips = Helptip.findAllByPublishedAndDomainName(true, attrs.domain)
       if (helptips) {
+
+        // include necessary resource modules or libraries
         if ( pluginManager.getGrailsPlugin('resources') ) {
-            out << "<r:require module=\"jquery-ui\"/>\n"
-        } else if (attrs.includeJQ==null || attrs.includeJQ==true) {
-            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-1.7.min.js' ) + "\"></script>\n"
-            out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'jquery-ui-1.8.15.custom.css' ) + "\"/>\n"
-            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery-ui-1.8.15.custom.min.js' ) + "\"></script>\n"
+          out << '<r:require module="domain-helptips" />'
+        } else {
+          if (attrs.includeJQ==null || attrs.includeJQ==true) {
+            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js/ext', file: 'jquery-1.7.min.js' ) + "\"></script>\n"
+            out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css/ext', file: 'jquery-ui-1.8.15.custom.css' ) + "\"/>\n"
+            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js/ext', file: 'jquery-ui-1.8.15.custom.min.js' ) + "\"></script>\n"
+          }
+          if (attrs.includeHI==null || attrs.includeHI==true) {
+            out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js/ext', file: 'jquery.hoverIntent.minified.js' ) + "\"></script>\n"
           }
         out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'jquery.bt.custom.js' ) + "\"></script>\n"
         out << "<link rel='stylesheet' href=\"" + g.resource(plugin: 'domain-helptips', dir: '/css', file: 'helptips.css' ) + "\"/>\n"
         out << "<script type=\"text/javascript\" src=\"" + g.resource(plugin: 'domain-helptips', dir: '/js', file: 'helptips.js' ) + "\"></script>\n"
+        }
+
+        // now write the js to create the helptips for this domain
         out << "<script type=\"text/javascript\">\n"
 
         out << """
@@ -39,7 +48,9 @@ helptips.updateDomainProperty = function(domainPropertyId,domainNameId) {
         helptips.each {
           out << "helptips.createTip(\"${it.id}\", \"${it.domainName}\", \"${it.domainProperty}\", \"${it.title?.encodeAsJavaScript()}\", \"${it.description?.encodeAsJavaScript()}\");\n"
           }
+
         out << "</script>\n"
+
       } else  {
         out << "<!-- No helptips for domain ${attrs.domain} -->\n"
       }
